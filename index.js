3272d9f6cb2f91f6ddf426406eba816c0e97cef1
Riyadulislamratul <riadul1810833319@gmail.com>
2026-02-02 23:12:13 +0600

    02/02/2026 pratice

---
+++ index.js
@@ -0,0 +1,8 @@
+// 1. Declare a variable and assign a number.
+let myNumber = 18 
+// 2. Declare a variable and assign a string.
+let myString = "I'm Riyadul Islam Ratul"
+// 3. Declare a variable and assign a boolean.
+let myBoolean = false
+// 4. Check the data type of a variable.
+console.log(myBoolean)
