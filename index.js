dadd7d8a52843bae61130ee5ed16d52fd957d663
Riyadulislamratul <riadul1810833319@gmail.com>
2026-02-08 23:11:07 +0600

    08/02/2026 pratice

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 // 1. Declare a variable and assign a number.
-let myNumber = 18 
+// let myNumber = 18 
+
+
 // 2. Declare a variable and assign a string.
-let myString = "I'm Riyadul Islam Ratul"
+// let myString = "I'm Riyadul Islam Ratul"
+
+
 // 3. Declare a variable and assign a boolean.
-let myBoolean = false
+// let myBoolean = false
+
+
 // 4. Check the data type of a variable.
-console.log(myBoolean)
+// console.log(myBoolean)
+
+
 // 9. Declare a constant and try to reassign it.
-const myConstant = 3.1416
+// const myConstant = 3.1416
+
+
+// 11.Check if a number is positive or negative.
+// const num = 5
+// if (num > 0) {
+//     console.log("Positive Number")
+// } else {
+//     console.log("Negative Number")
+// }
+
+// 13.Compare two numbers and print the larger one.
+// const num1 = 6
+// const num2 = 10
+// if (num1>num2) {
+//     console.log("Number one is Largest")
+// } else {
+//     console.log("Number two is Largest")
+// }
+
