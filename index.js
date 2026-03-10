7dfbb1ce3cff7a28006e6266ebf431b3d4d921c0
Riyadulislamratul <riadul1810833319@gmail.com>
2026-03-10 22:19:08 +0600

    10/03/2026

--- index.js
+++ index.js
@@ -83,11 +83,24 @@ if (value1 === value2) {
 
 
 // 27.Reverse a number using a loop.
-let number = 12345;
-let reversedNumber = 0;
-while (number > 0) {
-    let digit = number % 10;
-    reversedNumber = reversedNumber * 10 + digit;
-    number = Math.floor(number / 10);
+// let number = 12345;
+// let reversedNumber = 0;
+// while (number > 0) {
+//     let digit = number % 10;
+//     reversedNumber = reversedNumber * 10 + digit;
+//     number = Math.floor(number / 10);
+// }
+// console.log(`Reversed Number: ${reversedNumber}`);
+
+
+// 29.Print Fibonacci series up to n terms.
+let n = 10;
+let a = 0, b = 1, nextTerm;
+console.log("Fibonacci Series:");
+
+for (let i = 1; i <= n; i++) {
+    console.log(a);
+    nextTerm = a + b;
+    a = b;
+    b = nextTerm;
 }
-console.log(`Reversed Number: ${reversedNumber}`);
